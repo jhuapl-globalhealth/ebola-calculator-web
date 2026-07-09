f9b2038c4214deca70995e7602115107fc2bada7
Miles Stewart <miles.stewart@jhuapl.edu>
2026-07-09 14:06:08 -0400

    iOS web access issue

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1380383547" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3588033177" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
